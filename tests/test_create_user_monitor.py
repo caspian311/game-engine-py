de63d3b047ee0e718867757f41e97a7d266b38a2
Matt Todd <matt.c.todd@gmail.com>
2025-04-30 22:29:42 -0500

    added test for create_user_monitor

---
+++ tests/test_create_user_monitor.py
@@ -0,0 +1,16 @@
+import mock
+
+from game.data import DATA, GameState
+from game.player import Player
+from game.create_user_monitor import CreateUserMonitor
+from game.commands import CommandProcessor, Commands
+
+@mock.patch.object(CommandProcessor, "queue_command")
+def test_create_user_monitor_starts_a_battle_once_user_is_created(mock_queue_command):
+    test_object = CreateUserMonitor()
+    DATA.user = Player("dummy")
+    DATA.state.run_state = GameState.STARTING
+
+    test_object.refresh()
+
+    mock_queue_command.assert_called_once_with(Commands.START_BATTLE, [])
