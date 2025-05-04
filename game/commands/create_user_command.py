e3b4d5ebe2d5ef79d175482d11a95d21003267d8
Matt Todd <matt.c.todd@gmail.com>
2025-05-04 16:13:06 -0500

    fixed tests

--- game/commands/create_user_command.py
+++ game/commands/create_user_command.py
@@ -5,13 +5,15 @@ from game.player_generator import PlayerGenerator
 class CreateUserCommand(Command):
     def execute(self, arguments):
         player_name = arguments[0]
-        player_attack = arguments[1]
-        player_defense = arguments[2]
-        player_magic = arguments[3]
-        player_constitution = arguments[4]
-
         DATA.user = PlayerGenerator.generate_user_player(player_name)
-        DATA.user.override_attack(player_attack)
-        DATA.user.override_defense(player_defense)
-        DATA.user.override_magic(player_magic)
-        DATA.user.override_constitution(player_constitution)
+
+        if len(arguments) > 1:
+            player_attack = arguments[1]
+            player_defense = arguments[2]
+            player_magic = arguments[3]
+            player_constitution = arguments[4]
+
+            DATA.user.override_attack(player_attack)
+            DATA.user.override_defense(player_defense)
+            DATA.user.override_magic(player_magic)
+            DATA.user.override_constitution(player_constitution)
--- tests/commands/test_create_user_command.py
+++ tests/commands/test_create_user_command.py
@@ -7,3 +7,13 @@ def test_create_user_command_creates_user():
     CreateUserCommand().execute(["test 123"])
 
     assert "test 123" == DATA.user.name()
+
+def test_create_user_command_creates_user_with_given_attributes():
+    DATA.user = None
+
+    CreateUserCommand().execute(["test 123", 1, 2, 3, 4])
+
+    assert 1 == DATA.user.attack()
+    assert 2 == DATA.user.defense()
+    assert 3 == DATA.user.magic()
+    assert 4 == DATA.user.constitution()
