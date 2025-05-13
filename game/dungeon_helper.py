470571e96ee5ace9fc9a634fe4c8bf08beb20c1a
Matt Todd <matt.c.todd@gmail.com>
2025-05-13 07:14:02 -0500

    WIP

--- game/dungeon_helper.py
+++ game/dungeon_helper.py
@@ -20,22 +20,45 @@ def player_surroundings():
 
     player_x, player_y = player_position_in_dungeon()
 
-    print(f"player position: {player_x}, {player_y}")
-
     start_x = max(player_x - 5, 0)
     end_x = min(player_x + 6, dungeon_width)
 
-    print(f"x range: {start_x}:{end_x}")
-
     start_y = max(player_y - 5, 0)
     end_y = min(player_y + 6, dungeon_height)
-
-    print(f"y range: {start_y}:{end_y}")
 
     ret_val = []
     lines = dungeon_map()[start_y:end_y]
 
     for ret_line in lines:
         ret_val.append(ret_line[start_x:end_x])
+
+    return ret_val
+
+def blow_up_player_surroundings(width, height):
+    player_surr = player_surroundings()
+    height_of_player_surroundings = len(player_surr)
+    height_scale_factor = int(height_of_player_surroundings / height)
+
+    width_of_player_surroundings = len(player_surr[0])
+    width_scale_factor = int(width_of_player_surroundings / height)
+
+    ret_val = [[" " for x in range(width)] for y in range(height)]
+
+    for y, _ in enumerate(player_surr):
+        for x, _ in enumerate(player_surr[y]):
+            character = player_surr[y][x]
+            if character == "-":
+                x_offset_start = 0
+                y_offset_start = 0
+                x_offset_range = 0
+                y_offset_range = 0
+
+                for i in range(y_offset_start, y_offset_range):
+                    for j in range(x_offset_start, x_offset_range):
+                        ret_val[i][j] = "-"
+            elif character == "|":
+                pass
+
+
 
     return ret_val
--- tests/test_dungeon_helper.py
+++ tests/test_dungeon_helper.py
@@ -1,4 +1,7 @@
-from game.dungeon_helper import dungeon_map, player_position_in_dungeon, player_surroundings
+from game.dungeon_helper import (dungeon_map,
+                                 player_position_in_dungeon,
+                                 player_surroundings,
+                                 blow_up_player_surroundings)
 
 def test_dungeon_map_has_right_size():
     assert len(dungeon_map()) == 10
@@ -11,12 +14,48 @@ def test_player_position():
     assert x == 17
     assert y == 9
 
-def test_player_position_gives_5_columns_and_5_rows_on_either_side():
+def test_player_surroundings_gives_5_columns_and_5_rows_on_either_side():
     surroundings = player_surroundings()
 
     assert len(surroundings) == 6
     assert len(surroundings[0]) == 11
     assert "".join(surroundings[0]) == "-|     |---"
+    assert "".join(surroundings[1]) == " |  X  |   "
+    assert "".join(surroundings[2]) == " |-----|   "
+    assert "".join(surroundings[3]) == "           "
+    assert "".join(surroundings[4]) == "---|   |---"
+    assert "".join(surroundings[5]) == "   | * |   "
+
+def test_blow_up_player_surroundings_scale_to_given_width_height():
+    width = 100
+    height = 20
+    surroundings = blow_up_player_surroundings(width, height)
+
+    assert len(surroundings) == height
+    assert len(surroundings[0]) == width
+
+def test_blow_up_player_surroundings_scale_to_given_width_height2():
+    width = 200
+    height = 40
+    surroundings = blow_up_player_surroundings(width, height)
+
+    assert len(surroundings) == height
+    assert len(surroundings[0]) == width
+
+def test_blow_up_player_surroundings():
+    width = 120
+    height = 30
+    surroundings = blow_up_player_surroundings(width, height)
+
+    print("*******************************************************")
+    print("".join(surroundings[0]))
+    print("".join(surroundings[1]))
+    print("".join(surroundings[2]))
+    print("*******************************************************")
+
+    assert "".join(surroundings[0]) == "   |         "
+    assert "".join(surroundings[0]) == "---|     |---"
+    assert "".join(surroundings[0]) == "   |     |---"
     assert "".join(surroundings[1]) == " |  X  |   "
     assert "".join(surroundings[2]) == " |-----|   "
     assert "".join(surroundings[3]) == "           "
